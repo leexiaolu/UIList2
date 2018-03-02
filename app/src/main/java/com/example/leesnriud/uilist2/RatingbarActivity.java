91c1ad21c40c6e4e8c2ca751ac284fa0aade3dd8
leexiaolu <573702124@qq.com>
2018-03-02 10:59:17 +0800

    seekbar and ratingbar commit

--- app/src/main/java/com/example/leesnriud/uilist2/RatingbarActivity.java
+++ app/src/main/java/com/example/leesnriud/uilist2/RatingbarActivity.java
@@ -1,13 +1,37 @@
 package com.example.leesnriud.uilist2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * android ratingbar
+ * <p>
+ * android:isIndicator：是否用作指示，用户无法更改，默认false
+ * android:numStars：显示多少个星星，必须为整数
+ * android:rating：默认评分值，必须为浮点数
+ * android:stepSize： 评分每次增加的值，必须为浮点数
+ */
 public class RatingbarActivity extends AppCompatActivity {
+
+    @BindView(R.id.rb_ratingbar)
+    RatingBar rbRatingbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ratingbar);
+        ButterKnife.bind(this);
+
+        rbRatingbar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(RatingbarActivity.this,String.valueOf(v),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/leesnriud/uilist2/SeekbarActivity.java
+++ app/src/main/java/com/example/leesnriud/uilist2/SeekbarActivity.java
@@ -1,13 +1,58 @@
 package com.example.leesnriud.uilist2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * android seekbar
+ * <p>
+ * android:max="100" //滑动条的最大值
+ * android:progress="60" //滑动条的当前值
+ * android:secondaryProgress="70" //二级滑动条的进度
+ * android:thumb = "@mipmap/sb_icon" //滑块的drawable
+ * <p>
+ * SeekBar.OnSeekBarChangeListener 我们只需重写三个对应的方法
+ * onProgressChanged：进度发生改变时会触发
+ * onStartTrackingTouch：按住SeekBar时会触发
+ * onStopTrackingTouch：放开SeekBar时触发
+ */
 public class SeekbarActivity extends AppCompatActivity {
+
+    @BindView(R.id.sb_seebar)
+    SeekBar sbSeebar;
+    @BindView(R.id.tv_seekbar)
+    TextView tvSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seekbar);
+        ButterKnife.bind(this);
+        bindView();
+    }
+
+    private void bindView(){
+        sbSeebar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvSeekbar.setText("当前进度值:" + i + "  / 100 ");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(SeekbarActivity.this,"触碰SeekBar",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(SeekbarActivity.this,"放开SeekBar",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
